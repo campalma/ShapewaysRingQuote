8ec70c1915fe3d0b494463d11835df79918058d7
Camilo Palma <cpalmapradena@gmail.com>
2013-10-14 11:30:01 -0300

    Add to cart after login

--- Assets/Scripts/AccessTokenRequest.cs
+++ Assets/Scripts/AccessTokenRequest.cs
@@ -20,33 +20,36 @@ public class AccessTokenRequest : MonoBehaviour {
 	}
 	
 	IEnumerator getAccessToken(){
-			Dictionary<string,string> authParameters = OAuth.generateParams(ShapewaysKeys.consumerKey, userAccessToken);
-			authParameters.Add("oauth_verifier", verifier);
-			authParameters.Add("oauth_signature", OAuth.generateSignature(ShapewaysKeys.accessTokenUrl, "GET", authParameters, ShapewaysKeys.consumerKeySecret, userAccessTokenSecret));
-			
-			HTTP.Request accessTokenRequest = new HTTP.Request("GET", ShapewaysKeys.accessTokenUrl+OAuth.ToQueryString(authParameters));
-			Debug.Log(ShapewaysKeys.accessTokenUrl+OAuth.ToQueryString(authParameters));
-			accessTokenRequest.Send();
+		Dictionary<string,string> authParameters = OAuth.generateParams(ShapewaysKeys.consumerKey, userAccessToken);
+		authParameters.Add("oauth_verifier", verifier);
+		authParameters.Add("oauth_signature", OAuth.generateSignature(ShapewaysKeys.accessTokenUrl, "GET", authParameters, ShapewaysKeys.consumerKeySecret, userAccessTokenSecret));
 		
-			while(!accessTokenRequest.isDone) yield return new WaitForEndOfFrame();
-			
-			if (accessTokenRequest.exception != null)
-				Debug.LogError (accessTokenRequest.exception); 
-			else{
-				string response = accessTokenRequest.response.Text;
-				int index = response.IndexOf('=');
-				int end = response.IndexOf('&');
-				string accessToken = response.Substring(index + 1, end - index - 1);
-				index = response.IndexOf("secret");
-				string accessTokenSecret = response.Substring(index + 7);
-				PlayerPrefs.SetString("accessToken", accessToken);
-				Debug.Log(accessToken);
-				PlayerPrefs.SetString("accessTokenSecret", accessTokenSecret);
-				Debug.Log(accessTokenSecret);
-				Debug.Log(response);
-				GameObject confirmBuy = GameObject.Find("Buy");
-				confirmBuy.gameObject.transform.FindChild("SendObject").active = false;
-				confirmBuy.gameObject.transform.FindChild("SendObject").active = true;
-			}
+		HTTP.Request accessTokenRequest = new HTTP.Request("GET", ShapewaysKeys.accessTokenUrl+OAuth.ToQueryString(authParameters));
+		Debug.Log(ShapewaysKeys.accessTokenUrl+OAuth.ToQueryString(authParameters));
+		accessTokenRequest.Send();
+	
+	
+		while(!accessTokenRequest.isDone) yield return new WaitForEndOfFrame();
+		
+		if (accessTokenRequest.exception != null)
+			Debug.LogError (accessTokenRequest.exception); 
+	
+		else{
+			string response = accessTokenRequest.response.Text;
+			int index = response.IndexOf('=');
+			int end = response.IndexOf('&');
+			string accessToken = response.Substring(index + 1, end - index - 1);
+			index = response.IndexOf("secret");
+			string accessTokenSecret = response.Substring(index + 7);
+			PlayerPrefs.SetString("accessToken", accessToken);
+			PlayerPrefs.SetString("accessTokenSecret", accessTokenSecret);
+			Debug.Log(response);
+			GameObject confirmBuy = GameObject.Find("Buy(Clone)");
+			Debug.Log(confirmBuy);
+			Transform child = confirmBuy.gameObject.transform.FindChild("SendObjet");
+			Debug.Log(child);
+			child.active = false;
+			child.active = true;
+		}
 	}
 }
